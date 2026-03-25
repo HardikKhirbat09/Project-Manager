14a05924c56c876b8ebfe3a6c309ac2c6fe5d950
Hardik <hardikkhirbat942@gmail.com>
2026-03-26 00:16:43 +0530

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
+import { connectDB } from './db/index.js';
+
 dotenv.config({
     path: './.env',
 });
@@ -7,13 +10,24 @@ dotenv.config({
 //const  PORT = process.env.PORT || 5000; process.env is an object that contains the env variables from env file
 
 const app = express();
-
+//basic configuration
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+//cors configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((error) => {
+    console.error('Failed to connect to the database:', error);
+    process.exit(1); 
 });
