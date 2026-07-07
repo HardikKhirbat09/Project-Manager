559a5c9c8fff2562b6424661a57b0b3caa445387
Hardik <hardikkhirbat942@gmail.com>
2026-07-08 03:53:46 +0530

    bug fixed

--- backend/src/utils/mails.js
+++ backend/src/utils/mails.js
@@ -15,7 +15,6 @@ const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host : process.env.SMTP_HOST,
         port : process.env.SMTP_PORT,
-        secure : true,
         auth : {
             user : process.env.SMTP_USER,
             pass : process.env.SMTP_PASS,
